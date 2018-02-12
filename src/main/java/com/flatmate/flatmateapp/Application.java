2145eedb77f6b257cf0a3acfb73e217b51df1e93
alistairspence <alistaaairspence@gmail.com>
2018-02-12 23:20:02 +0000

    Testing OAuth2 with Facebook login

--- src/main/java/com/flatmate/flatmateapp/Application.java
+++ src/main/java/com/flatmate/flatmateapp/Application.java
@@ -2,8 +2,10 @@ package com.flatmate.flatmateapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableOAuth2Sso
 @SpringBootApplication
 @ComponentScan
 public class Application {
