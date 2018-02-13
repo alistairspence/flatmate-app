73a68c7e8a129539dec61597c7c79e9eee989293
alistairspence <alistaaairspence@gmail.com>
2018-02-13 20:58:26 +0000

    Removed unnecessary import

--- src/main/java/com/flatmate/flatmateapp/AccountController.java
+++ src/main/java/com/flatmate/flatmateapp/AccountController.java
@@ -1,7 +1,6 @@
 package com.flatmate.flatmateapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
