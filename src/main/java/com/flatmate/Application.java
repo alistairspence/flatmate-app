6535746670a7b023de3027c1e7f212339ff38a12
alistairspence <alistaaairspence@gmail.com>
2018-02-13 22:03:08 +0000

    Removed OAuth2

--- src/main/java/com/flatmate/Application.java
+++ src/main/java/com/flatmate/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//@EnableOAuth2Sso
 @SpringBootApplication
 @ComponentScan
 public class Application {
