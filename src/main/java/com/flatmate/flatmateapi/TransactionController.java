103c1533d35084bf31f5fcc73c7c5450ae9c5020
alistairspence <alistaaairspence@gmail.com>
2018-06-20 22:09:31 +0100

    Global CORS

--- src/main/java/com/flatmate/flatmateapi/TransactionController.java
+++ src/main/java/com/flatmate/flatmateapi/TransactionController.java
@@ -3,11 +3,13 @@ package com.flatmate.flatmateapi;
 import com.flatmate.flatmatepersistence.Transaction;
 import com.flatmate.flatmateregistry.TransactionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -26,15 +28,18 @@ public class TransactionController {
 
 	@ResponseBody
 	@RequestMapping(value = "/{transactionId}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
 	public Transaction getTransactionById(@PathVariable final Long transactionId) { return transactionService.getTransactionById(transactionId); }
 
 	@RequestMapping(method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
 	public List<Transaction> getAllTransactions() {
 		return transactionService.getAllTransactions();
 	}
 
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
 	public Transaction createTransaction(@RequestBody final Transaction transaction) {
 		transaction.setLocalDateTime(LocalDateTime.now());
 		return transactionService.createTransaction(transaction);
@@ -42,11 +47,13 @@ public class TransactionController {
 
 	@ResponseBody
 	@RequestMapping(value = "/{transactionId}", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
 	public Transaction updateTransaction(@PathVariable final Long transactionId, @RequestBody final Transaction transaction) {
 		return transactionService.updateTransaction(transactionId, transaction);
 	}
 
 	@RequestMapping(value = "/{transactionId}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteTransaction(@PathVariable final Long transactionId) { transactionService.deleteTransaction(transactionId); }
 
 }
