d09ec9b5fd4e835d01e523d120745ce14a1d1c02
alistairspence <alistaaairspence@gmail.com>
2018-07-28 00:08:16 +0100

    - updated the build file - attempting to try through localhost

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 cd
 rm -rf flatmate
-git clone https://github.com/alistairspence/flatmate-app flatmate
-cd flatmate
+git clone https://github.com/alistairspence/flatmate-app
+cd flatmate-app/
 mvn clean install
-cd target
+cd target/
+java -jar flatmateapp-0.0.1-SNAPSHOT.jar
 
