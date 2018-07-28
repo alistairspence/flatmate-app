ba06909e9fbf35880399f6ba79a8df4402f4d724
alistairspence <alistaaairspence@gmail.com>
2018-07-28 15:09:54 +0100

    - removing html from project folder

--- src/main/resources/static/Scripts/Time.js
+++
@@ -1,7 +0,0 @@
-
-
-
-var d = new Date();
-var y = d.getFullYear();
-document.getElementById("demo").innerHTML = y;
-
--- src/main/resources/static/Scripts/testRedirect.js
+++
@@ -1,6 +0,0 @@
-function checkUserPass() //function that is used for displaying and storing
-{
-
-	window.location.href = "../Webpage/dashboard_.html";
-
-}//end of function
