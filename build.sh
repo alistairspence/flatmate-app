883c46349b8f1424b867336063ba93f263d46185
alistairspence <alistaaairspence@gmail.com>
2018-07-28 16:07:40 +0100

    - updated application.properties to allow remote connections - updated build file to overwrite properties

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ git clone https://github.com/alistairspence/flatmate-app
 cd flatmate-app/
 mvn clean install
 cd target/
-java -jar flatmateapp-0.0.1-SNAPSHOT.jar
-
+java -jar flatmateapp-0.0.1-SNAPSHOT.jar --spring.datasource.url=jdbc:mysql://127.0.0.1:3306/testdb --spring.datasource.username=root --spring.datasource.password=root
